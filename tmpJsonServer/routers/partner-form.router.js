177ebb3f7d4618e999cf26e1f31ce0eb088e7104
Mahmut Akyol <mahmut.akyol@outlook.com.tr>
2018-12-14 16:08:35 +0300

    refactoring

--- tmpJsonServer/routers/partner-form.router.js
+++ tmpJsonServer/routers/partner-form.router.js
@@ -4,6 +4,8 @@ const jsonServer = require('json-server');
 const lodashId = require('lodash-id');
 const mixins = require('../mixins');
 
+const dbHelper = require('../helpers/db.helper');
+
 const server = jsonServer.create();
 const routerx = jsonServer.router('db.json');
 
@@ -15,18 +17,18 @@ server.get('/', getAllPartnerFormsCB);
 server.post('/', postPartnerDataCB);
 
 function getAllPartnerFormsCB(req, res, nex) {
-	res.status(200).json(db.get('partners').value());
+	res.status(200).json(dbHelper.getDataByFieldName('partners'));
 }
 
 function postPartnerDataCB(req, res, next) {
 
-	var items = db.get('partners').value();
+	var items = dbHelper.getDataByFieldName('partners');
 
 	items.push(req.body);
 
 	db.write();
 
-	res.status(200).json(db.get('partners').value());
+	res.status(200).json('ok');
 }
 
 module.exports = server;
