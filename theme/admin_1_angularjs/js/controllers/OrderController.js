482efc10903d4b9ad1d0f4ad1f09365f98172284
Mahmut Akyol <mahmut.akyol@outlook.com.tr>
2018-12-25 16:59:09 +0300

    Closed #11

--- theme/admin_1_angularjs/js/controllers/OrderController.js
+++ theme/admin_1_angularjs/js/controllers/OrderController.js
@@ -18,43 +18,42 @@
         vm.objectTypes = globalData.objektTypen;
         vm.orderTypes = globalData.auftragsart;
 
-        passDataService.setObj(
-            {
-                orderDatas: {
-                    customer: {
-                        customerNumber: "BXP-105",
-                        isCompany: "company",
-                        salutation: "Mr.",
-                        title: "Prof. Dr.",
-                        firstName: "Mahmut",
-                        lastName: "Akyol",
-                        companyName: "Compact Management",
-                        additive: "baubasis",
-                        street: "Mecidiyekoy st.",
-                        country: "Turkey",
-                        postcode: "34000",
-                        city: "Istanbul",
-                        phone: "+90 212 212 12 12",
-                        mobile: "+90 553 364 04 74",
-                        email: "mahmut.akyol@bauexperts.de"
-                    },
-                    object: {
-                        objectNumber: 0,
-                        objectType: "Einfamilienhaus",
-                        street: "Acıbadem st.",
-                        country: "Turkey",
-                        postcode: "34000",
-                        city: "Istanbul"
-                    },
-                    otherInformations: {
-                        orderNumber: "BXP-ORD-0000-256",
-                        orderDate: "20.12.2018",
-                        referenceNumber: "BXP-REF-005",
-                        orderType: globalData.auftragsart[0]
-                    }
-                }
+        vm.baseData = passDataService.getObj();
+        vm.baseData.orderDatas = {
+            customer: {
+                customerNumber: "BXP-105",
+                isCompany: "company",
+                salutation: "Mr.",
+                title: "Prof. Dr.",
+                firstName: "Mahmut",
+                lastName: "Akyol",
+                companyName: "Compact Management",
+                additive: "baubasis",
+                street: "Mecidiyekoy st.",
+                country: "Turkey",
+                postcode: "34000",
+                city: "Istanbul",
+                phone: "+90 212 212 12 12",
+                mobile: "+90 553 364 04 74",
+                email: "mahmut.akyol@bauexperts.de"
+            },
+            object: {
+                objectNumber: 0,
+                objectType: "Einfamilienhaus",
+                street: "Acıbadem st.",
+                country: "Turkey",
+                postcode: "34000",
+                city: "Istanbul"
+            },
+            otherInformations: {
+                orderNumber: "BXP-ORD-0000-256",
+                orderDate: "20.12.2018",
+                referenceNumber: "BXP-REF-005",
+                orderType: globalData.auftragsart[0]
             }
-        );
+        };
+
+        passDataService.setObj(vm.baseData);
 
         init();
 
@@ -63,7 +62,7 @@
             vm.order = passDataService.getObj().orderDatas;
 
             console.log('====================================');
-            console.log(vm.order.object.objectType);
+            console.log(vm.baseData);
             console.log('====================================');
 
         }
--- theme/admin_1_angularjs/js/controllers/ProjectController.js
+++ theme/admin_1_angularjs/js/controllers/ProjectController.js
@@ -5,152 +5,150 @@
 		.module('MetronicApp')
 		.controller('ProjectController', ProjectController);
 
-        ProjectController.$inject = ['$rootScope', '$scope', '$state', 'DTOptionsBuilder', 'DTColumnDefBuilder', 'modalService'];
+        ProjectController.$inject = ['$rootScope', '$scope', '$state', 'DTOptionsBuilder', 'DTColumnDefBuilder', 'modalService', 'passDataService'];
 
 	/* @ngInject */
-	function ProjectController($rootScope, $scope, $state, DTOptionsBuilder, DTColumnDefBuilder, modalService) {
+	function ProjectController($rootScope, $scope, $state, DTOptionsBuilder, DTColumnDefBuilder, modalService, passDataService) {
 		// console.log("ProjectController Loaded");
 		var vm = this;
 
         vm.closeModal = closeModal;
         vm.submitForm = submitForm;
 
-        vm.documents = [
-            {
-                number: 1,
-                date: '18.12.2018',
-                status: 'status',
-                title: 'foo',
-                coverPicUrl: 'https://www.picsum.photos/200/300',
-                detection: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                detail: {
-                    id: 8743,
-                    date: '18.12.2018',
-                    testField: {
-                        id: "008",
-                        name: "Wasserhaltungsarbeiten",
-                        group:"ROHBAU"
-                    },
-                    position: 'position',
-                    title: 'title',
-                    evaluation: {
-                        id: "03",
-                        name: "Abweichung von den allg. anerkannten Regeln der Technik(a.a.R.d.T.)",
-                        shortcut: "AT"
-                    },
-                    basics: {
-                        id: "03",
-                        name: "Baubeschreibung"
-                    },
-                    status: {
-                        id: "03",
-                        name: "Mengelbeseitigung schriftlich vom BT / GU / NU bestätig und durch den Sachverständigen kontrolliert"
-                    },
-                    description: 'desc',
-                    costs: {
-                        disposalCost: 'disposal',
-                        impairment: 'imp',
-                        recoup: 'stopaj',
-                        isPrint: true
-                    }
-                }
-            },
-            {
-                number: 2,
-                date: '18.12.2018',
-                status: 'status',
-                title: 'foo',
-                coverPicUrl: 'https://www.picsum.photos/200/300',
-                detection: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                detail: {
-                    id: 8744,
-                    date: '18.12.2018',
-                    testField: {
-                        id: "008",
-                        name: "Wasserhaltungsarbeiten",
-                        group:"ROHBAU"
-                    },
-                    position: 'position',
-                    title: 'title',
-                    evaluation: {
-                        id: "03",
-                        name: "Abweichung von den allg. anerkannten Regeln der Technik(a.a.R.d.T.)",
-                        shortcut: "AT"
-                    },
-                    basics: {
-                        id: "03",
-                        name: "Baubeschreibung"
-                    },
-                    status: {
-                        id: "03",
-                        name: "Mengelbeseitigung schriftlich vom BT / GU / NU bestätig und durch den Sachverständigen kontrolliert"
-                    },
-                    description: 'desc',
-                    costs: {
-                        disposalCost: 'disposal',
-                        impairment: 'imp',
-                        recoup: 'stopaj',
-                        isPrint: true
-                    }
-                }
-            },
-            {
-                number: 3,
-                date: '18.12.2018',
-                status: 'status',
-                title: 'foo',
-                coverPicUrl: 'https://www.picsum.photos/200/300',
-                detection: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
-                detail: {
-                    id: 8745,
-                    date: '18.12.2018',
-                    testField: {
-                        id: "008",
-                        name: "Wasserhaltungsarbeiten",
-                        group:"ROHBAU"
-                    },
-                    position: 'position',
-                    title: 'title',
-                    evaluation: {
-                        id: "03",
-                        name: "Abweichung von den allg. anerkannten Regeln der Technik(a.a.R.d.T.)",
-                        shortcut: "AT"
-                    },
-                    basics: {
-                        id: "03",
-                        name: "Baubeschreibung"
-                    },
-                    status: {
-                        id: "03",
-                        name: "Mengelbeseitigung schriftlich vom BT / GU / NU bestätig und durch den Sachverständigen kontrolliert"
-                    },
-                    description: 'desc',
-                    costs: {
-                        disposalCost: 'disposal',
-                        impairment: 'imp',
-                        recoup: 'stopaj',
-                        isPrint: true
-                    }
-                }
-            }
-        ];
-
-        vm.project = {
+        vm.baseData = passDataService.getObj();
+        vm.baseData.projectDatas = {
             projectNumber: "BXP-PRN-001",
             projectName: "Project Name Field",
             ownPerformanceBuilder: "",
             documents: [
-              {
-                path: "",
-                title: ""
-              },
-              {
-                path: "",
-                title: ""
-              }
+                {
+                    number: 1,
+                    date: '18.12.2018',
+                    status: 'status',
+                    title: 'foo',
+                    coverPicUrl: 'https://www.picsum.photos/200/300',
+                    detection: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    detail: {
+                        id: 8743,
+                        date: '18.12.2018',
+                        testField: {
+                            id: "008",
+                            name: "Wasserhaltungsarbeiten",
+                            group:"ROHBAU"
+                        },
+                        position: 'position',
+                        title: 'title',
+                        evaluation: {
+                            id: "03",
+                            name: "Abweichung von den allg. anerkannten Regeln der Technik(a.a.R.d.T.)",
+                            shortcut: "AT"
+                        },
+                        basics: {
+                            id: "03",
+                            name: "Baubeschreibung"
+                        },
+                        status: {
+                            id: "03",
+                            name: "Mengelbeseitigung schriftlich vom BT / GU / NU bestätig und durch den Sachverständigen kontrolliert"
+                        },
+                        description: 'desc',
+                        costs: {
+                            disposalCost: 'disposal',
+                            impairment: 'imp',
+                            recoup: 'stopaj',
+                            isPrint: true
+                        }
+                    }
+                },
+                {
+                    number: 2,
+                    date: '18.12.2018',
+                    status: 'status',
+                    title: 'foo',
+                    coverPicUrl: 'https://www.picsum.photos/200/300',
+                    detection: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    detail: {
+                        id: 8744,
+                        date: '18.12.2018',
+                        testField: {
+                            id: "008",
+                            name: "Wasserhaltungsarbeiten",
+                            group:"ROHBAU"
+                        },
+                        position: 'position',
+                        title: 'title',
+                        evaluation: {
+                            id: "03",
+                            name: "Abweichung von den allg. anerkannten Regeln der Technik(a.a.R.d.T.)",
+                            shortcut: "AT"
+                        },
+                        basics: {
+                            id: "03",
+                            name: "Baubeschreibung"
+                        },
+                        status: {
+                            id: "03",
+                            name: "Mengelbeseitigung schriftlich vom BT / GU / NU bestätig und durch den Sachverständigen kontrolliert"
+                        },
+                        description: 'desc',
+                        costs: {
+                            disposalCost: 'disposal',
+                            impairment: 'imp',
+                            recoup: 'stopaj',
+                            isPrint: true
+                        }
+                    }
+                },
+                {
+                    number: 3,
+                    date: '18.12.2018',
+                    status: 'status',
+                    title: 'foo',
+                    coverPicUrl: 'https://www.picsum.photos/200/300',
+                    detection: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+                    detail: {
+                        id: 8745,
+                        date: '18.12.2018',
+                        testField: {
+                            id: "008",
+                            name: "Wasserhaltungsarbeiten",
+                            group:"ROHBAU"
+                        },
+                        position: 'position',
+                        title: 'title',
+                        evaluation: {
+                            id: "03",
+                            name: "Abweichung von den allg. anerkannten Regeln der Technik(a.a.R.d.T.)",
+                            shortcut: "AT"
+                        },
+                        basics: {
+                            id: "03",
+                            name: "Baubeschreibung"
+                        },
+                        status: {
+                            id: "03",
+                            name: "Mengelbeseitigung schriftlich vom BT / GU / NU bestätig und durch den Sachverständigen kontrolliert"
+                        },
+                        description: 'desc',
+                        costs: {
+                            disposalCost: 'disposal',
+                            impairment: 'imp',
+                            recoup: 'stopaj',
+                            isPrint: true
+                        }
+                    }
+                }
             ],
             intenalNotes: ""
-        };
+        };  
+
+        passDataService.setObj(vm.baseData);
+
+        init();
+
+        function init() {
+            vm.project = passDataService.getObj().projectDatas;
+        }
 
         function dblClick(document) {
 
